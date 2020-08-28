9e0ab4589d908b168f46869c423037e91fc4d2e5
Vojtech Trefny <vtrefny@redhat.com>
2020-08-28 08:34:43 +0200

    Add setup.py and basic module structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2020  Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup, Extension
+
+def main():
+    setup(name="pyblkid",
+          version="0.1",
+          description="Python interface for the libblkid C library",
+          author="Vojtech Trefny",
+          author_email="vtrefny@redhat.com",
+          ext_modules=[Extension("blkid",
+                                 sources = ["src/pyblkid.c"],
+                                 include_dirs = ["/usr/include"],
+                                 libraries = ["blkid"],
+                                 library_dirs = ["/usr/lib"])])
+
+if __name__ == "__main__":
+    main()
---
+++ src/pyblkid.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020  Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "pyblkid.h"
+
+#include <blkid/blkid.h>
+
+static PyMethodDef BlkidMethods[] = {
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef blkidmodule = {
+    PyModuleDef_HEAD_INIT,
+    "blkid",
+    "Python interface for the libblkid C library",
+    -1,
+    BlkidMethods
+};
+
+PyMODINIT_FUNC PyInit_blkid (void) {
+    PyObject *module = NULL;
+
+    module = PyModule_Create (&blkidmodule);
+    if (!module)
+        return NULL;
+
+    return module;
+}
